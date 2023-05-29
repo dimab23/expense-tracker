684c9aced30c57c7679e5dab86e32947c2014270
dimab <Genius.23>
2023-05-29 10:31:41 +0300

    change licence

--- src/main/java/com/expense/tracker/config/OpenApi30Config.java
+++ src/main/java/com/expense/tracker/config/OpenApi30Config.java
@@ -1,7 +1,7 @@
 /*
     MIT License
 
-    Copyright (c) 2023 Beșelea Dumitru
+    Copyright (c) 2023 Beșelea Dumitru & Șaptefrați Victor
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
  */
+
 package com.expense.tracker.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
